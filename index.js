106348d84a1b91e8c1220e5ed412743a0250c143
RadGade <rishiadhikari5@gmail.com>
2020-01-14 08:38:00 +1030

    try 4

--- index.js
+++ index.js
@@ -3,20 +3,9 @@ const torrentStream = require('torrent-stream')
 const bodyParser = require('body-parser');
 const {  getmagLink, search, listmovie,} = require('./scrapper/scrapper.js')
 const app = express()
-var socket = require('socket.io')
-var ss = require('socket.io-stream');
-var io = socket(server)
 var os = require('os');
 var DIR = os.tmpdir()+'/torrent-web-poc';
 let connections = []
-
-io.on('connection', (socket) => {
-	connections.push(socket)
-	console.log('connected: %s sockets connected', connections.length)
-	socket.on('add-torrent', addTorrent);
-	//disconect
-	socket.on('watch', addTorrent)
-})
 
 
 
@@ -63,27 +52,3 @@ app.get('/watch/:link', async (req, res) => {
 	 })
 	})
 })
-
-
-function addTorrent(incoming) {
-	var magLink = incoming
-try {
-	var engine = torrentStream(magLink, {
-		 uploads: 3,
-		 connections: 30,
-		 path: DIR
-	 })
-	engine.on('ready', () => {
-	 engine.files.forEach(async (file) => {
-		 if (file.name.endsWith('.mp4')) {
-			 console.log('filename:', file.name);
-			 var stream = file.createReadStream()
-			 res.writeHead(200, {'Content-Type' : 'video/mp4'})
-			 stream.pipe(res);
-		 }
-	 })
-	})
-} catch (e) {
-	console.log(e);
-}
-}
